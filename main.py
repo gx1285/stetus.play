5d20a626422cd47a200eca01590def565caf6cfa
gx1285JP（日本ですよ？） <78240988+gx1285JP@users.noreply.github.com>
2021-06-12 10:03:23 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,9 @@ import os
 import discord
 from server import keep_alive
 
-client = discord.Client(activity=discord.Game(name='my game'))
+client = discord.Client()
 
-# or, for watching:
-activity = discord.Activity(name='起動中', type=discord.ActivityType.watching)
-client = discord.Client(activity=activity)
+
 
 keep_alive()
 
